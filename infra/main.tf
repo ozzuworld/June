744d5807630a1e9b5875f71cf160efe163201564
kkazuma <you@example.com>
2025-09-04 18:41:10 -0500

    1

--- infra/main.tf
+++ infra/main.tf
@@ -17,13 +17,13 @@ resource "google_project_service" "apis" {
 ############################################
 # Artifact Registry
 ############################################
-resource "google_artifact_registry_repository" "repo" {
-  project       = var.project_id
-  location      = var.region
-  repository_id = var.repo_name
-  format        = "DOCKER"
-  depends_on    = [google_project_service.apis]
-}
+##resource "google_artifact_registry_repository" "repo" {
+##  project       = var.project_id
+##  location      = var.region
+##  repository_id = var.repo_name
+##  format        = "DOCKER"
+##  depends_on    = [google_project_service.apis]
+##}
 
 ############################################
 # STT service
