9d209937f003a91de9b72ef26fe1633e91356004
kkazuma <you@example.com>
2025-09-05 12:50:27 -0500

    4

--- infra/variables-june-auth.tf
+++ infra/variables-june-auth.tf
@@ -16,13 +16,45 @@ variable "june_auth_allow_unauthenticated" {
 }
 
 # App config
-variable "june_auth_totp_issuer"         { type = string, default = "June Voice" }
-variable "june_auth_totp_alg"            { type = string, default = "SHA1" }
-variable "june_auth_totp_digits"         { type = number, default = 6 }
-variable "june_auth_totp_period"         { type = number, default = 30 }
-variable "june_auth_mfa_jwt_ttl_seconds" { type = number, default = 600 }
+variable "june_auth_totp_issuer" {
+  type    = string
+  default = "June Voice"
+}
+
+variable "june_auth_totp_alg" {
+  type    = string
+  default = "SHA1"
+}
+
+variable "june_auth_totp_digits" {
+  type    = number
+  default = 6
+}
+
+variable "june_auth_totp_period" {
+  type    = number
+  default = 30
+}
+
+variable "june_auth_mfa_jwt_ttl_seconds" {
+  type    = number
+  default = 600
+}
 
 # Optional: Create secret versions via Terraform (leaks to TF state if used)
-variable "june_auth_create_secret_versions" { type = bool, default = false }
-variable "june_auth_fernet_key"            { type = string, sensitive = true, default = null }
-variable "june_auth_mfa_jwt_secret"        { type = string, sensitive = true, default = null }
+variable "june_auth_create_secret_versions" {
+  type    = bool
+  default = false
+}
+
+variable "june_auth_fernet_key" {
+  type      = string
+  sensitive = true
+  default   = null
+}
+
+variable "june_auth_mfa_jwt_secret" {
+  type      = string
+  sensitive = true
+  default   = null
+}
