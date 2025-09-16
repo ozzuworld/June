c9238f29c09d753cba433c8ea0580ca1823ad575
kkazuma <you@example.com>
2025-09-16 13:10:00 -0500

    migration to k8s

--- terraform/ci/artifact_registry.tf
+++
@@ -1,8 +0,0 @@
-
-resource "google_artifact_registry_repository" "june" {
-  project       = var.project_id
-  location      = var.region
-  repository_id = "june"
-  format        = "DOCKER"
-  description   = "Container images for June services"
-}
--- terraform/ci/outputs.tf
+++
@@ -1,7 +0,0 @@
-
-output "wif_provider_resource" {
-  value = "projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.github_provider.workload_identity_pool_provider_id}"
-}
-output "deployer_sa_email" {
-  value = google_service_account.github_deployer.email
-}
