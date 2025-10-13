e7064dce49c5bc79280ff787187528bfbf0d4e2a
ozzuworld <eng.ozzu@icloud.com>
2025-10-13 13:41:46 -0500

    Fix ROOT_DIR syntax error

--- scripts/install/02-docker.sh
+++ scripts/install/02-docker.sh
@@ -7,7 +7,7 @@ set -e
 source "$(dirname "$0")/../common/logging.sh"
 source "$(dirname "$0")/../common/validation.sh"
 
-ROOT_DIR="${1:-$(dirname "$(dirname "$(dirname "$0")")")"}" 
+ROOT_DIR="${1:-$(dirname $(dirname $(dirname $0)))}"
 
 install_docker() {
     log "Phase 2/9: Installing Docker..."
