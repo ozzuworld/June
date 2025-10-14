01c407485b1e648572ab7579406d17051d5efa04
ozzuworld <eng.ozzu@icloud.com>
2025-10-14 16:20:05 -0500

    Update services __init__.py to export LiveKit service

--- June/services/june-orchestrator/app/services/__init__.py
+++ June/services/june-orchestrator/app/services/__init__.py
@@ -1,6 +1,12 @@
-"""
-Business Logic Services
-"""
-from . import ai_service, tts_service, stt_service
+"""Service layer exports"""
+from .ai_service import ai_service
+from .stt_service import stt_service
+from .tts_service import tts_service
+from .livekit_service import livekit_service
 
-__all__ = ["ai_service", "tts_service", "stt_service"]
+__all__ = [
+    "ai_service",
+    "stt_service", 
+    "tts_service",
+    "livekit_service"
+]
